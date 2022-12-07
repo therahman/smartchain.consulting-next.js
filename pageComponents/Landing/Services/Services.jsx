bca911696aa39a4f1b5cc1d10fed9b2eaab90519
Shanur Rahman <shanur@smartchain.me>
2022-12-07 12:55:14 +0400

    css changes

--- pageComponents/Landing/Services/Services.jsx
+++ pageComponents/Landing/Services/Services.jsx
@@ -18,7 +18,7 @@ const Services = () => {
 					</div>
 					<div className='pb-6 mt-3'>
 						<h1 className='text-lg text-secondary font-semibold'>Mobile App Development</h1>
-						<p className='text-dark-400 text-base'>
+						<p>
 							Our society is primarily based on mobile devices. Modern businesses frequently demand some
 							kind of mobile app. With a focus on mobile-first design, Smart Chain Technology team can
 							create and publish cutting-edge hybrid or native apps across all app stores.
@@ -36,7 +36,7 @@ const Services = () => {
 					</div>
 					<div className='pb-6 mt-3'>
 						<h1 className='text-lg text-secondary font-semibold'>NFT Development</h1>
-						<p className='text-dark-400 text-base'>
+						<p>
 							Global company Smart Chain Technology supports companies in reimagining their industries for
 							the digital era. Products and services from Smart Chain Technology are founded on
 							innovation, a strong culture of taking risks, and a tireless commitment to the needs of the
@@ -56,7 +56,7 @@ const Services = () => {
 					</div>
 					<div className='pb-6 mt-3'>
 						<h1 className='text-lg text-secondary font-semibold'>WEB APP DEVELOPMENT</h1>
-						<p className='text-dark-400 text-base'>
+						<p>
 							Simple websites to intricate, scalable platforms have all been developed by Smart Chain
 							Technology. Smart Chain can put together the ideal combination of web technologies based on
 							the particular requirements and objectives of its clients to make sure that their products
@@ -75,7 +75,7 @@ const Services = () => {
 					</div>
 					<div className=' pb-6 mt-3'>
 						<h1 className='text-lg text-secondary font-semibold'>CUSTOM SOFTWARE</h1>
-						<p className='text-dark-400 text-base'>
+						<p>
 							No matter the sector, Smart Chain collaborates closely with clients globally to deliver
 							full-spectrum, unique software solutions that wow. Smart Chain offers the next generation in
 							custom software development services. We've built full product suites and MVPs, therefore we
@@ -94,7 +94,7 @@ const Services = () => {
 					</div>
 					<div className=' pb-6 mt-3'>
 						<h1 className='text-lg text-secondary font-semibold'>WEBSITE DEVELOPMENT</h1>
-						<p className='text-dark-400 text-base'>
+						<p>
 							​We create award-winning websites using excellent technology and exceptional taste. These
 							are what determine whether or not your customer responds to what you're presenting. As a
 							creative digital agency, Smart Chain Technology plans out a startling website development
@@ -113,7 +113,7 @@ const Services = () => {
 					</div>
 					<div className=' pb-6 mt-3'>
 						<h1 className='text-lg text-secondary font-semibold'>DIGITAL MARKETING</h1>
-						<p className='text-dark-400 text-base'>
+						<p>
 							The future depends on technology, and technology depends on marketing. Therefore, now is the
 							ideal time to start your digital adventure and take the first steps toward cutting-edge
 							marketing trends.
